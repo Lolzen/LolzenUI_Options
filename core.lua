b7f71c91c3920956d9eadfda9db798c688f56f6f
Georg Eitler <funer@gmx.at>
2017-11-28 20:43:28 +0100

    core: add LSM support for statusbars
    new pickertype "statusbar"

--- core.lua
+++ core.lua
@@ -148,12 +148,14 @@ ns.createPicker = function(module, pickertype, name, width, selected)
 		t = ns.picker_flags
 	elseif pickertype == "bin" then
 		t = ns.picker_bin
+	elseif pickertype == "statusbar" then
+		t = LSM:List(LSM.MediaType.STATUSBAR)
 	end
 	local selectedNum, selectedName
 	local picker = CreateFrame("Button", name, ns[module], "UIDropDownMenuTemplate")
 	picker:Show()
 	local function OnClick(name)
-		if pickertype == "font" then
+		if pickertype == "font" or pickertype == "statusbar" then
 			UIDropDownMenu_SetSelectedName(picker, name.value)
 		else
 			UIDropDownMenu_SetSelectedID(picker, name:GetID())
@@ -176,7 +178,7 @@ ns.createPicker = function(module, pickertype, name, width, selected)
 	UIDropDownMenu_Initialize(picker, initialize)
 	UIDropDownMenu_SetWidth(picker, width)
 	UIDropDownMenu_SetButtonWidth(picker, width+15)
-	if pickertype == "font" then
+	if pickertype == "font" or pickertype == "statusbar" then
 		UIDropDownMenu_SetSelectedName(picker, selectedName)
 	else
 		UIDropDownMenu_SetSelectedID(picker, selectedNum)
@@ -190,7 +192,7 @@ ns.createColorTexture = function(module, width, height, colorVars, texture)
 	local colorTex = ns[module]:CreateTexture(nil, "ARTWORK")
 	colorTex:SetSize(width, height)
 	colorTex:SetVertexColor(unpack(colorVars))
-	colorTex:SetTexture("Interface\\AddOns\\LolzenUI\\media\\"..texture)
+	colorTex:SetTexture(LSM:Fetch("statusbar", texture))
 	return colorTex
 end
 

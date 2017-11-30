8f1b1b635f9bf5244b555337bd7c3b29bbf58790
Georg Eitler <funer@gmx.at>
2017-12-01 00:05:51 +0100

    unitframes: initialize sub-optionpanels for player, target, targettarget, party raid and pet

--- modules/unitframes/options.lua
+++ modules/unitframes/options.lua
@@ -107,6 +107,66 @@ f:SetScript("OnEvent", function(self, event, addon)
 		local fadout_alpha = ns.createPicker("unitframes", "alpha", "uf_fadout_alpha", 45, LolzenUIcfg.unitframes["uf_fade_outofreach_alpha"])
 		fadout_alpha:SetPoint("LEFT", fadeout_alpha_text, "RIGHT", -10, -3)
 		
+		-- // Create a subcategory panel for Player // --
+		ns.uf_player_options = CreateFrame("Frame", "unitframe_playerpanel", ns["unitframes"])
+		ns.uf_player_options.name = "   Player"
+		ns.uf_player_options.parent = "unitframes"
+		InterfaceOptions_AddCategory(ns.uf_player_options)
+
+		local player_title = ns.uf_player_options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		player_title:SetPoint("TOPLEFT", ns.uf_player_options, 16, -16)
+		player_title:SetText("|cff5599ffUnitframes module: Player Options|r")
+		
+		-- // Create a subcategory panel for Target // --
+		ns.uf_target_options = CreateFrame("Frame", "unitframe_targetpanel", ns["unitframes"])
+		ns.uf_target_options.name = "   Target"
+		ns.uf_target_options.parent = "unitframes"
+		InterfaceOptions_AddCategory(ns.uf_target_options)
+
+		local target_title = ns.uf_target_options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		target_title:SetPoint("TOPLEFT", ns.uf_target_options, 16, -16)
+		target_title:SetText("|cff5599ffUnitframes module: Target Options|r")
+		
+		-- // Create a subcategory panel for Targettarget // --
+		ns.uf_targettarget_options = CreateFrame("Frame", "unitframe_targettargetpanel", ns["unitframes"])
+		ns.uf_targettarget_options.name = "   Targettarget"
+		ns.uf_targettarget_options.parent = "unitframes"
+		InterfaceOptions_AddCategory(ns.uf_targettarget_options)
+
+		local targettarget_title = ns.uf_targettarget_options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		targettarget_title:SetPoint("TOPLEFT", ns.uf_targettarget_options, 16, -16)
+		targettarget_title:SetText("|cff5599ffUnitframes module: Targettarget Options|r")
+		
+		-- // Create a subcategory panel for Party // --
+		ns.uf_party_options = CreateFrame("Frame", "unitframe_partypanel", ns["unitframes"])
+		ns.uf_party_options.name = "   Party"
+		ns.uf_party_options.parent = "unitframes"
+		InterfaceOptions_AddCategory(ns.uf_party_options)
+
+		local party_title = ns.uf_party_options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		party_title:SetPoint("TOPLEFT", ns.uf_party_options, 16, -16)
+		party_title:SetText("|cff5599ffUnitframes module: Party Options|r")
+		
+		-- // Create a subcategory panel for Raid // --
+		ns.uf_raid_options = CreateFrame("Frame", "unitframe_raidpanel", ns["unitframes"])
+		ns.uf_raid_options.name = "   Raid"
+		ns.uf_raid_options.parent = "unitframes"
+		InterfaceOptions_AddCategory(ns.uf_raid_options)
+
+		local raid_title = ns.uf_raid_options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		raid_title:SetPoint("TOPLEFT", ns.uf_raid_options, 16, -16)
+		raid_title:SetText("|cff5599ffUnitframes module: Raid Options|r")
+		
+		-- // Create a subcategory panel for Pet // --
+		ns.uf_pet_options = CreateFrame("Frame", "unitframe_petpanel", ns["unitframes"])
+		ns.uf_pet_options.name = "   Pet"
+		ns.uf_pet_options.parent = "unitframes"
+		InterfaceOptions_AddCategory(ns.uf_pet_options)
+
+		local pet_title = ns.uf_pet_options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		pet_title:SetPoint("TOPLEFT", ns.uf_pet_options, 16, -16)
+		pet_title:SetText("|cff5599ffUnitframes module: Pet Options|r")
+		
 		ns["unitframes"].okay = function(self)
 			LolzenUIcfg.unitframes["uf_use_hp_percent"] = cb1:GetChecked()
 			LolzenUIcfg.unitframes["uf_use_sivalue"] = cb2:GetChecked()
@@ -137,6 +197,48 @@ f:SetScript("OnEvent", function(self, event, addon)
 			LolzenUIcfg.unitframes["uf_lead_anchor"] = "TOPLEFT"
 			LolzenUIcfg.unitframes["uf_fade_outofreach"] = true
 			LolzenUIcfg.unitframes["uf_fade_outofreach_alpha"] = 0.5
+			ReloadUI()
+		end
+		
+		ns["uf_player_options"].okay = function(self)
+		end
+		
+		ns["uf_player_options"].default = function(self)
+			ReloadUI()
+		end
+		
+		ns["uf_target_options"].okay = function(self)
+		end
+		
+		ns["uf_target_options"].default = function(self)
+			ReloadUI()
+		end
+		
+		ns["uf_targettarget_options"].okay = function(self)
+		end
+		
+		ns["uf_targettarget_options"].default = function(self)
+			ReloadUI()
+		end
+		
+		ns["uf_party_options"].okay = function(self)
+		end
+		
+		ns["uf_party_options"].default = function(self)
+			ReloadUI()
+		end
+		
+		ns["uf_raid_options"].okay = function(self)
+		end
+		
+		ns["uf_raid_options"].default = function(self)
+			ReloadUI()
+		end
+		
+		ns["uf_pet_options"].okay = function(self)
+		end
+		
+		ns["uf_pet_options"].default = function(self)
 			ReloadUI()
 		end
 	end

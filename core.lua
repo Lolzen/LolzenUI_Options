4307c44180fde7d9abb369c0bd96c8dc223a836a
Georg Eitler <funer@gmx.at>
2018-06-17 18:14:58 +0200

    core: add icon previews to statusbar & background pickers
    along with a referebce list for BfA

--- core.lua
+++ core.lua
@@ -209,6 +209,49 @@ ns.picker_uf_auragrowth_y = {
 	"DOWN",
 }
 
+--[[
+List of button attributes
+======================================================
+info.text = [STRING]  --  The text of the button
+info.value = [ANYTHING]  --  The value that UIDROPDOWNMENU_MENU_VALUE is set to when the button is clicked
+info.func = [function()]  --  The function that is called when you click the button
+info.checked = [nil, true, function]  --  Check the button if true or function returns true
+info.isNotRadio = [nil, true]  --  Check the button uses radial image if false check box image if true
+info.isTitle = [nil, true]  --  If it's a title the button is disabled and the font color is set to yellow
+info.disabled = [nil, true]  --  Disable the button and show an invisible button that still traps the mouseover event so menu doesn't time out
+info.tooltipWhileDisabled = [nil, 1] -- Show the tooltip, even when the button is disabled.
+info.hasArrow = [nil, true]  --  Show the expand arrow for multilevel menus
+info.hasColorSwatch = [nil, true]  --  Show color swatch or not, for color selection
+info.r = [1 - 255]  --  Red color value of the color swatch
+info.g = [1 - 255]  --  Green color value of the color swatch
+info.b = [1 - 255]  --  Blue color value of the color swatch
+info.colorCode = [STRING] -- "|cAARRGGBB" embedded hex value of the button text color. Only used when button is enabled
+info.swatchFunc = [function()]  --  Function called by the color picker on color change
+info.hasOpacity = [nil, 1]  --  Show the opacity slider on the colorpicker frame
+info.opacity = [0.0 - 1.0]  --  Percentatge of the opacity, 1.0 is fully shown, 0 is transparent
+info.opacityFunc = [function()]  --  Function called by the opacity slider when you change its value
+info.cancelFunc = [function(previousValues)] -- Function called by the colorpicker when you click the cancel button (it takes the previous values as its argument)
+info.notClickable = [nil, 1]  --  Disable the button and color the font white
+info.notCheckable = [nil, 1]  --  Shrink the size of the buttons and don't display a check box
+info.owner = [Frame]  --  Dropdown frame that "owns" the current dropdownlist
+info.keepShownOnClick = [nil, 1]  --  Don't hide the dropdownlist after a button is clicked
+info.tooltipTitle = [nil, STRING] -- Title of the tooltip shown on mouseover
+info.tooltipText = [nil, STRING] -- Text of the tooltip shown on mouseover
+info.tooltipOnButton = [nil, 1] -- Show the tooltip attached to the button instead of as a Newbie tooltip.
+info.justifyH = [nil, "CENTER"] -- Justify button text
+info.arg1 = [ANYTHING] -- This is the first argument used by info.func
+info.arg2 = [ANYTHING] -- This is the second argument used by info.func
+info.fontObject = [FONT] -- font object replacement for Normal and Highlight
+info.menuTable = [TABLE] -- This contains an array of info tables to be displayed as a child menu
+info.noClickSound = [nil, 1]  --  Set to 1 to suppress the sound when clicking the button. The sound only plays if .func is set.
+info.padding = [nil, NUMBER] -- Number of pixels to pad the text on the right side
+info.leftPadding = [nil, NUMBER] -- Number of pixels to pad the button on the left side
+info.minWidth = [nil, NUMBER] -- Minimum width for this line
+info.customFrame = frame -- Allows this button to be a completely custom frame, should inherit from UIDropDownCustomMenuEntryTemplate and override appropriate methods.
+info.icon = [TEXTURE] -- An icon for the button.
+info.mouseOverIcon = [TEXTURE] -- An override icon when a button is moused over.
+https://github.com/tomrus88/BlizzardInterfaceCode/blob/master/Interface/FrameXML/UIDropDownMenu.lua
+]]
 local LolzenFontObjects = {}
 ns.createPicker = function(module, pickertype, name, width, selected, sub)
 	local t
@@ -272,6 +315,12 @@ ns.createPicker = function(module, pickertype, name, width, selected, sub)
 				LolzenFontObjects[v]:SetFont(LSM:Fetch("font", v), 12, "")
 				--now use created fontObjects to distinguish the fonts in the picker
 				info.fontObject = v
+			elseif pickertype == "statusbar" then
+				info.icon = LSM:Fetch("statusbar", v)
+				info.tCoord = 0, 1, 1, 1
+			elseif pickertype == "background" then
+				info.icon = LSM:Fetch("background", v)
+				info.tCoord = 0, 1, 1, 1
 			end
 			UIDropDownMenu_AddButton(info, level)
 		end

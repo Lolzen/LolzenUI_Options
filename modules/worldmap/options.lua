ed956489e502b9aae249708b17c0d616be28b814
Georg Eitler <funer@gmx.at>
2020-06-26 15:31:21 +0200

    worldmap: realtime options

--- modules/worldmap/options.lua
+++ modules/worldmap/options.lua
@@ -19,28 +19,49 @@ f:SetScript("OnEvent", function(self, event, addon)
 		local scale = ns.createInputbox("worldmap", 30, 20, LolzenUIcfg.worldmap["worldmap_scale"])
 		scale:SetPoint("LEFT", scale_text, "RIGHT", 10, 0)
 
+		scale:SetScript("OnEscapePressed", function(self)
+			self:SetText(self.oldText)
+			self:ClearFocus()
+		end)
+
+		scale:SetScript("OnEnterPressed", function(self)
+			LolzenUIcfg.worldmap["worldmap_scale"] = tonumber(scale:GetText())
+			self.oldText = self:GetText()
+		end)
+
+		scale:SetScript("OnEditFocusGained", function(self)
+			self.oldText = self:GetText()
+		end)
+
+		scale:SetScript("OnEditFocusLost", function(self)
+			self:SetText(self.oldText)
+			self:ClearFocus()
+		end)
+
 		local cb1 = ns.createCheckBox("worldmap", "title_coordinates", "|cff5599ff"..L["wm_coordinates"].."|r", LolzenUIcfg.worldmap["worldmap_coordinates"])
 		cb1:SetPoint("TOPLEFT", scale_text, "BOTTOMLEFT", 0, -8)
+		cb1:SetScript("OnClick", function(self)
+			LolzenUIcfg.worldmap["worldmap_coordinates"] = cb1:GetChecked()
+			LolzenUI.setWMCoordinates()
+		end)
 
 		local color_text = ns.createFontstring("worldmap", L["wm_title_or_coordinates_color"]..":")
 		color_text:SetPoint("TOPLEFT", cb1, "BOTTOMLEFT", 0, -8)
 
 		local color = ns.createColorTexture("worldmap", 16, 16, LolzenUIcfg.worldmap["worldmap_title_color"], "LolzenUI Standard")
 		color:SetPoint("LEFT", color_text, "RIGHT", 10, 0)
+		color.setActualColors = function()
+			LolzenUIcfg.worldmap["worldmap_title_color"] = {color:GetVertexColor()}
+			LolzenUI.setWMTitleColor()
+		end
 
 		local color_f = ns.createColorPicker("worldmap", color, LolzenUIcfg.worldmap["worldmap_title_color"])
 		color_f:SetAllPoints(color)
 
 		local cb2 = ns.createCheckBox("worldmap", "save_worldmap_position", "|cff5599ff"..L["save_worldmap_position"].."|r", LolzenUIcfg.worldmap["worldmap_save_position"])
 		cb2:SetPoint("TOPLEFT", color_text, "BOTTOMLEFT", 0, -8)
-
-		local applyButton = ns.createApplyButton("worldmap")
-		applyButton:SetScript("OnClick", function()
-			LolzenUIcfg.worldmap["worldmap_scale"] = tonumber(scale:GetText())
-			LolzenUIcfg.worldmap["worldmap_coordinates"] = cb1:GetChecked()
-			LolzenUIcfg.worldmap["worldmap_title_color"] = {color:GetVertexColor()}
+		cb2:SetScript("OnClick", function(self)
 			LolzenUIcfg.worldmap["worldmap_save_position"] = cb2:GetChecked()
-			ReloadUI()
 		end)
 
 		ns["worldmap"].default = function(self)

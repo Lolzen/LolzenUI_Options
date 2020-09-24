d51fc550e8cbc8a15b7344063196ee232462709c
Georg Eitler <funer@gmx.at>
2020-09-24 15:18:56 +0200

    worldmap: adjustments

--- modules/worldmap/options.lua
+++ modules/worldmap/options.lua
@@ -18,7 +18,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		aboutBG:SetFrameLevel(1)
 
 		local scale_text = ns.createFontstring("worldmap", L["wm_scale"]..":")
-		scale_text:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -26)
+		scale_text:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 4, -26)
 
 		local scale = ns.createPicker("worldmap", "alpha", "worldmapframe_scale", 45, LolzenUIcfg.worldmap["worldmap_scale"])
 		scale:SetPoint("LEFT", scale_text, "RIGHT", -10, -3)
@@ -28,14 +28,14 @@ f:SetScript("OnEvent", function(self, event, addon)
 		end
 
 		local cb1 = ns.createCheckBox("worldmap", "title_coordinates", "|cff5599ff"..L["wm_coordinates"].."|r", LolzenUIcfg.worldmap["worldmap_coordinates"])
-		cb1:SetPoint("TOPLEFT", scale_text, "BOTTOMLEFT", 0, -8)
+		cb1:SetPoint("TOPLEFT", scale_text, "BOTTOMLEFT", -4, -8)
 		cb1:SetScript("OnClick", function(self)
 			LolzenUIcfg.worldmap["worldmap_coordinates"] = cb1:GetChecked()
 			LolzenUI.setWMCoordinates()
 		end)
 
 		local color_text = ns.createFontstring("worldmap", L["wm_title_or_coordinates_color"]..":")
-		color_text:SetPoint("TOPLEFT", cb1, "BOTTOMLEFT", 0, -8)
+		color_text:SetPoint("TOPLEFT", cb1, "BOTTOMLEFT", 4, -8)
 
 		local color = ns.createColorTexture("worldmap", 16, 16, LolzenUIcfg.worldmap["worldmap_title_color"], "LolzenUI Standard")
 		color:SetPoint("LEFT", color_text, "RIGHT", 10, 0)
@@ -48,7 +48,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		color_f:SetAllPoints(color)
 
 		local cb2 = ns.createCheckBox("worldmap", "save_worldmap_position", "|cff5599ff"..L["save_worldmap_position"].."|r", LolzenUIcfg.worldmap["worldmap_save_position"])
-		cb2:SetPoint("TOPLEFT", color_text, "BOTTOMLEFT", 0, -8)
+		cb2:SetPoint("TOPLEFT", color_text, "BOTTOMLEFT", -4, -8)
 		cb2:SetScript("OnClick", function(self)
 			LolzenUIcfg.worldmap["worldmap_save_position"] = cb2:GetChecked()
 		end)

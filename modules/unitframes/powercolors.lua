3b5d6c0a8cc5af94b62df40ca31930c195b7c1e3
Georg Eitler <funer@gmx.at>
2020-10-22 12:07:51 +0200

    unitframes: powercolors: realtime options

--- modules/unitframes/powercolors.lua
+++ modules/unitframes/powercolors.lua
@@ -20,6 +20,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local mana = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[0], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		mana:SetPoint("LEFT", mana_text, "RIGHT", 100, 0)
+		mana.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[0] = {mana:GetVertexColor()}
+			LolzenUI.SetUFPowerColorMana()
+		end
 
 		local mana_f = ns.createColorPicker("uf_powercolor_options", mana, LolzenUIcfg.unitframes.powercolors[0])
 		mana_f:SetAllPoints(mana)
@@ -29,6 +33,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local rage = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[1], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		rage:SetPoint("TOP", mana, "BOTTOM", 0, -4)
+		rage.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[1] = {rage:GetVertexColor()}
+			LolzenUI.SetUFPowerColorRage()
+		end
 
 		local rage_f = ns.createColorPicker("uf_powercolor_options", rage, LolzenUIcfg.unitframes.powercolors[1])
 		rage_f:SetAllPoints(rage)
@@ -38,6 +46,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local focus = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[2], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		focus:SetPoint("TOP", rage, "BOTTOM", 0, -4)
+		focus.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[2] = {focus:GetVertexColor()}
+			LolzenUI.SetUFPowerColorFocus()
+		end
 
 		local focus_f = ns.createColorPicker("uf_powercolor_options", focus, LolzenUIcfg.unitframes.powercolors[2])
 		focus_f:SetAllPoints(focus)
@@ -47,6 +59,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local energy = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[3], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		energy:SetPoint("TOP", focus, "BOTTOM", 0, -4)
+		energy.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[3] = {energy:GetVertexColor()}
+			LolzenUI.SetUFPowerColorEnergy()
+		end
 
 		local energy_f = ns.createColorPicker("uf_powercolor_options", energy, LolzenUIcfg.unitframes.powercolors[3])
 		energy_f:SetAllPoints(energy)
@@ -56,6 +72,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local cp = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[4], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		cp:SetPoint("TOP", energy, "BOTTOM", 0, -4)
+		cp.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[4] = {cp:GetVertexColor()}
+			LolzenUI.SetUFPowerColorCP()
+		end
 
 		local cp_f = ns.createColorPicker("uf_powercolor_options", cp, LolzenUIcfg.unitframes.powercolors[4])
 		cp_f:SetAllPoints(cp)
@@ -65,6 +85,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local runes = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[5], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		runes:SetPoint("TOP", cp, "BOTTOM", 0, -4)
+		runes.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[5] = {runes:GetVertexColor()}
+			LolzenUI.SetUFPowerColorRunes()
+		end
 
 		local runes_f = ns.createColorPicker("uf_powercolor_options", runes, LolzenUIcfg.unitframes.powercolors[5])
 		runes_f:SetAllPoints(runes)
@@ -74,6 +98,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local rp = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[6], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		rp:SetPoint("TOP", runes, "BOTTOM", 0, -4)
+		rp.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[6] = {rp:GetVertexColor()}
+			LolzenUI.SetUFPowerColorRunePower()
+		end
 
 		local rp_f = ns.createColorPicker("uf_powercolor_options", rp, LolzenUIcfg.unitframes.powercolors[6])
 		rp_f:SetAllPoints(rp)
@@ -83,6 +111,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local soulshards = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[7], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		soulshards:SetPoint("TOP", rp, "BOTTOM", 0, -4)
+		soulshards.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[7] = {soulshards:GetVertexColor()}
+			LolzenUI.SetUFPowerColorSoulshards()
+		end
 
 		local soulshards_f = ns.createColorPicker("uf_powercolor_options", soulshards, LolzenUIcfg.unitframes.powercolors[7])
 		soulshards_f:SetAllPoints(soulshards)
@@ -92,6 +124,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local lp = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[8], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		lp:SetPoint("TOP", soulshards, "BOTTOM", 0, -4)
+		lp.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[8] = {lp:GetVertexColor()}
+			LolzenUI.SetUFPowerColorLunarPower()
+		end
 
 		local lp_f = ns.createColorPicker("uf_powercolor_options", lp, LolzenUIcfg.unitframes.powercolors[8])
 		lp_f:SetAllPoints(lp)
@@ -101,6 +137,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local hp = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[9], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		hp:SetPoint("TOP", lp, "BOTTOM", 0, -4)
+		hp.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[9] = {hp:GetVertexColor()}
+			LolzenUI.SetUFPowerColorHolyPower()
+		end
 
 		local hp_f = ns.createColorPicker("uf_powercolor_options", hp, LolzenUIcfg.unitframes.powercolors[9])
 		hp_f:SetAllPoints(hp)
@@ -110,6 +150,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local maelstrom = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[11], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		maelstrom:SetPoint("TOP", hp, "BOTTOM", 0, -4)
+		maelstrom.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[11] = {maelstrom:GetVertexColor()}
+			LolzenUI.SetUFPowerColorMaelstrom()
+		end
 
 		local maelstrom_f = ns.createColorPicker("uf_powercolor_options", maelstrom, LolzenUIcfg.unitframes.powercolors[11])
 		maelstrom_f:SetAllPoints(maelstrom)
@@ -119,6 +163,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local chi = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[12], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		chi:SetPoint("TOP", maelstrom, "BOTTOM", 0, -4)
+		chi.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[12] = {chi:GetVertexColor()}
+			LolzenUI.SetUFPowerColorChi()
+		end
 
 		local chi_f = ns.createColorPicker("uf_powercolor_options", chi, LolzenUIcfg.unitframes.powercolors[12])
 		chi_f:SetAllPoints(chi)
@@ -128,6 +176,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local insanity = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[13], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		insanity:SetPoint("TOP", chi, "BOTTOM", 0, -4)
+		insanity.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[13] = {insanity:GetVertexColor()}
+			LolzenUI.SetUFPowerColorInsanity()
+		end
 
 		local insanity_f = ns.createColorPicker("uf_powercolor_options", insanity, LolzenUIcfg.unitframes.powercolors[13])
 		insanity_f:SetAllPoints(insanity)
@@ -137,6 +189,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local ac = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[16], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		ac:SetPoint("TOP", insanity, "BOTTOM", 0, -4)
+		ac.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[16] = {ac:GetVertexColor()}
+			LolzenUI.SetUFPowerColorArcaneCharges()
+		end
 
 		local ac_f = ns.createColorPicker("uf_powercolor_options", ac, LolzenUIcfg.unitframes.powercolors[16])
 		ac_f:SetAllPoints(ac)
@@ -146,6 +202,10 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local fury = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[17], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		fury:SetPoint("TOP", ac, "BOTTOM", 0, -4)
+		fury.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[17] = {fury:GetVertexColor()}
+			LolzenUI.SetUFPowerColorFury()
+		end
 
 		local fury_f = ns.createColorPicker("uf_powercolor_options", fury, LolzenUIcfg.unitframes.powercolors[17])
 		fury_f:SetAllPoints(fury)
@@ -155,30 +215,13 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local pain = ns.createColorTexture("uf_powercolor_options", 16, 16, LolzenUIcfg.unitframes.powercolors[18], LolzenUIcfg.unitframes.general["uf_statusbar_texture"])
 		pain:SetPoint("TOP",fury, "BOTTOM", 0, -4)
+		pain.setActualColors = function()
+			LolzenUIcfg.unitframes.powercolors[18] = {pain:GetVertexColor()}
+			LolzenUI.SetUFPowerColorPain()
+		end
 
 		local pain_f = ns.createColorPicker("uf_powercolor_options", pain, LolzenUIcfg.unitframes.powercolors[18])
 		pain_f:SetAllPoints(pain)
-
-		local applyButton = ns.createApplyButton("uf_powercolor_options")
-		applyButton:SetScript("OnClick", function()
-			LolzenUIcfg.unitframes.powercolors[0] = {mana:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[1] = {rage:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[2] = {focus:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[3] = {energy:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[4] = {cp:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[5] = {runes:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[6] = {rp:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[7] = {soulshards:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[8] = {lp:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[9] = {hp:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[11] = {maelstrom:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[12] = {chi:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[13] = {insanity:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[16] = {ac:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[17] = {fury:GetVertexColor()}
-			LolzenUIcfg.unitframes.powercolors[18] = {pain:GetVertexColor()}
-			ReloadUI()
-		end)
 
 		ns["uf_powercolor_options"].default = function(self)
 			LolzenUIcfg.unitframes.powercolors = _G["LolzenUIdefaultcfg"].unitframes.powercolors
